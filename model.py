759b6d4cd29e706e00feabc73693903675748dbf
minh132 <trinhxuanminh1322002@gmai.com>
2022-11-28 22:57:43 +0700

    Build model

---
+++ model.py
@@ -0,0 +1,19 @@
+import torchvision.models as models
+import torch.nn as nn
+def build_model(pretrained=True, fine_tune=False, num_classes=10):
+    """Sử dụng mô hình pretrained  MobileNetV3 Large"""
+    if pretrained:
+        print('[INFO]: Loading pre-trained weights')
+    else:
+        print('[INFO]: Not loading pre-trained weights')
+    model = models.mobilenet_v3_large(pretrained=pretrained)
+    if fine_tune:
+        print('[INFO]: Fine-tuning all layers...')
+        for params in model.parameters():
+            params.requires_grad = True
+    elif not fine_tune:
+        print('[INFO]: Freezing hidden layers...')
+        for params in model.parameters():
+            params.requires_grad = False
+    model.classifier[3] = nn.Linear(in_features=1280, out_features=num_classes)
+    return model
